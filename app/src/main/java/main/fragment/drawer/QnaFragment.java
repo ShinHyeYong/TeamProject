4c37007dfa84d21877ec3fd9a90c5c6e48f50b1c
HY <954006@naver.com>
2016-12-05 02:12:20 +0900

    Send Email

--- app/src/main/java/main/fragment/drawer/QnaFragment.java
+++ app/src/main/java/main/fragment/drawer/QnaFragment.java
@@ -1,14 +1,23 @@
 package main.fragment.drawer;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import psj.hahaha.R;
 
-public class QnaFragment extends Fragment {
+public class QnaFragment extends Fragment implements View.OnClickListener {
+
+    EditText et;
+
+    private final String MAIL_TO = "shy954006@gmail.com";
+    private final String MAIL_TITLE = "~~에 관해 문의 드립니다.";
 
     public QnaFragment() {
         // Required empty public constructor
@@ -26,11 +35,31 @@ public class QnaFragment extends Fragment {
         super.onCreate(savedInstanceState);
     }
 
+    Button button;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this main.fragment
-        return inflater.inflate(R.layout.fragment_qna, container, false);
+        setHasOptionsMenu(true);
+        View fragment1View = inflater.inflate(R.layout.fragment_qna, container, false);
+        button = (Button) fragment1View.findViewById(R.id.bt_help);
+        et = (EditText) fragment1View.findViewById(R.id.et);
+
+        button.setOnClickListener(this);
+        return fragment1View;
     }
 
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.bt_help) {
+            // 메일내용
+            String text = et.getText().toString();
+            String mail = "mailto:" + MAIL_TO;
+            Intent intent = new Intent(Intent.ACTION_SENDTO, Uri.parse(mail));
+            intent.putExtra(Intent.EXTRA_SUBJECT, MAIL_TITLE);
+            intent.putExtra(Intent.EXTRA_TEXT, text);
+
+            this.startActivity(intent);
+        }
+    }
 }
