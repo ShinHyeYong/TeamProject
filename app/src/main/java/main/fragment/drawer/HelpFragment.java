80d6e6fb524f33a8d2f63afd631f9e57e3edbe44
yeot125 <yeot125@naver.com>
2016-12-09 15:39:16 +0900

    사용법 동영상 추가.

--- app/src/main/java/main/fragment/drawer/HelpFragment.java
+++ app/src/main/java/main/fragment/drawer/HelpFragment.java
@@ -30,7 +30,20 @@ public class HelpFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this main.fragment
-        return inflater.inflate(R.layout.fragment_help, container, false);
+        
+        View fragment2View = inflater.inflate(R.layout.fragment_help, container, false);
+        
+        LinearLayout layout = (LinearLayout) fragment2View.findViewById(R.id.fragment_help);
+        VideoView videoView = new VideoView(getActivity());
+        videoView.setVideoPath("http://180.229.55.20:30334/helpvid.mp4");
+        videoView.setLayoutParams(new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                                                               ViewGroup.LayoutParams.MATCH_PARENT));
+        layout.addView(videoView);
+        
+        videoView.start();
+        
+        return fragment2View;
+
     }
 
 }
